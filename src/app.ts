7e9f7a357e82ed42caa51c143905f830d8b8188a
mobilemancer <andrkarl@gmail.com>
2016-01-19 00:09:50 +0100

    Worked on equipment grid

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ export class App {
   router: Router;
   
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'Droid Works';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
+    //   { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+    //   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
       { route: 'droid',         name: 'droid',        moduleId: 'views/droid',  nav: true, title: 'Droid' }
     ]);
 
